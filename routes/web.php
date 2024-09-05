19d6758fe3bb7b25d2536654d54d1d5584fbf625
Mahdi <mahdinourizadegan.pro@gmail.com>
2024-09-05 09:51:44 +0330

    completed routes and made controller for app

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,19 @@
 <?php
 
+use App\Http\Controllers\Admin\CommentController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\PanelController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\SingertController;
+use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MenuContentController;
+use App\Http\Controllers\PostContentController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\SingerController;
+use App\Http\Controllers\User\FavortieController;
+use App\Http\Controllers\User\PanelController as UserPanelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +42,44 @@ Route::middleware('guest')->group(function() {
         Route::get('/store', [AuthController::class , 'register_store'])->name('register_store');
     });
 });
+
+Route::middleware('auth')->get('logout', [AuthController::class, 'logout']);
+
+Route::middleware('admin')->prefix('admin')->group(function() {
+    Route::get('/',[PanelController::class, 'index']);
+    Route::resource('/posts', PostController::class);
+    Route::resource('/tags', TagController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/singers', SingertController::class);
+    Route::resource('/comments', CommentController::class);
+    
+    Route::group(['prefix'=>'/users'],function() {
+        Route::get('/', [UserController::class, 'index']);
+        Route::delete('/delete/{id}', [UserController::class, 'delete']);
+        Route::post('/permission', [UserController::class, 'permission']);
+    });
+});
+
+Route::middleware('auth')->prefix('dashboard')->group(function() {
+    Route::get('/', [UserPanelController::class, 'index']);
+    
+    Route::resource('/tags', TagController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/singers', SingertController::class);
+    Route::resource('/comments', CommentController::class);
+    
+    Route::group(['prefix'=>'/favorites'],function() {
+        Route::get('/store', [FavortieController::class, 'index']);
+        Route::delete('/delete/{id}', [FavortieController::class, 'delete']);
+    });
+
+    Route::group(['prefix'=>'/info'],function() {
+        Route::get('/update', [FavortieController::class, 'index']);
+    });
+    
+});
+
+Route::get('/search', SearchController::class);
+Route::get('/menu/{menu}', MenuContentController::class);
+Route::get('/detail/{post}', PostContentController::class);
+Route::get('/singer/{id}', SingerController::class);
